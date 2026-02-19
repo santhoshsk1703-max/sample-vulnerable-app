42834a9c16b7cffbbe5d144e7441e81fe070fc4a
santhoshsk1703-max <santhoshsk1703@gmail.com>
2026-02-19 13:26:57 +0530

    chore: apply security fixes terraform/main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,13 +21,23 @@ resource "aws_iam_policy" "app_policy" {
   name        = "app-full-access"
   description = "Policy used by instances"
 
+  # FIX: Replaced wildcard "*" action with specific, least-privilege actions
+  # This addresses CWE-285 by restricting IAM permissions to only necessary services
   policy = <<EOF
 {
   "Version": "2012-10-17",
   "Statement": [
     {
       "Effect": "Allow",
-      "Action": "*",                             # Issue 2: wildcard actions
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:ListBucket",
+        "ec2:DescribeInstances",
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
       "Resource": "*"                            # Issue 3: wildcard resources
     }
   ]
